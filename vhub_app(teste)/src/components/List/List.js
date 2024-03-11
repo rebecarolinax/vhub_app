cea81ae10cc34c826e8d52b414e25412faac8338
Rebeca <rebecacarolina75@gmail.com>
2024-03-11 14:22:15 -0300

    VitalHub

--- vhub_app(teste)/src/components/List/List.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-import { FlatList } from "react-native";
-
-export const ListComponent = styled(FlatList)`
-     width: 100%;
-`
--- vhub_app(teste)/src/components/UserContainer/Style.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-
-export const UserContainer = styled.Image`
-    width: 100%;
-    height: 280px;
-`
